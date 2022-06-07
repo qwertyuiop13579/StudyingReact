c9d8c51383613b73276cc3118c8e4706017b391e
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-07 17:42:22 +0300

    add input to form

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -4,16 +4,29 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends React.Component {
 
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (event) => {
+        this.setState({ label: event.target.value });
+    }
+
+    onSubmit = (event) => {
+        this.props.AddItem(this.state.label);
+        event.preventDefault();
+        this.setState({ label: '' });
+    }
 
     render() {
-
-        const { AddItem } = this.props;
         return (
-            <div className="item-add-form">
-                <button onClick={() => AddItem('New Item')} type="button" className="btn btn-outline-secondary">
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to done?"
+                    value={this.state.label}></input>
+                <button type="submit" className="btn btn-outline-secondary">
                     Add
                 </button>
-            </div>
+            </form>
         );
     }
 }
