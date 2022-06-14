faf8d8bfa044f3058b1d73c7a6a4994185ad3e2e
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-14 13:45:20 +0300

    add spinner

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SwapiService from '../../services/swapi-service';
+import Spinner from "../spinner/spinner";
 
 import './random-planet.css';
 
@@ -10,19 +11,20 @@ class RandomPlanet extends React.Component {
 
     state = {
         planet: {},
+        isLoading: true,
     }
 
-    constructor() {
-        super();
+    // constructor() {
+    //     super();
+    //     this.updatePlanet();
+    // }
+
+    componentDidMount() {
         this.updatePlanet();
     }
 
-    //     componentDidMount() {
-    //         this.updatePlanet();
-    //    }
-
     onPlanetLoaded = (planet) => {
-        this.setState({ planet });
+        this.setState({ planet, isLoading: false });
     }
 
     updatePlanet() {
@@ -33,32 +35,46 @@ class RandomPlanet extends React.Component {
     }
 
     render() {
-        const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
+        const { planet, isLoading } = this.state;
+
+        const spinner = isLoading ? <Spinner /> : null;
+        const content = !isLoading ? <PlanetView planet={planet}></PlanetView> : null;
+
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet" />
-                <div>
-                    <h4>{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spinner}
+                {content}
             </div>
         )
     }
 
+}
+
+const PlanetView = ({ planet }) => {
+    const { id, name, population, rotationPeriod, diameter } = planet;
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet" />
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default RandomPlanet;
---
+++ star-db/src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-dz0itgiv2ab"><div className="ldio-kzys5fpszhg">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    );
+}
+
+export default Spinner;
