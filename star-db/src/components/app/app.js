231fdf4b6d2d34e46383c4b4ec357ae2856cb2b5
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-17 17:19:33 +0300

    use react context

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -9,6 +9,7 @@ import PeoplePage from "../people-page/people-page";
 import SwapiService from "../../services/swapi-service";
 import { PersonList, PlanetList, StarshipList } from "../sw-components/item-lists";
 import { PersonDetails, PlanetDetails, StarshipDetails } from "../sw-components/details";
+import { SwapiServiceProvider } from "../swapi-service-context";
 
 class App extends React.Component {
 
@@ -26,21 +27,21 @@ class App extends React.Component {
         if (this.state.hasError) return <ErrorIndicator />;
 
         return (
-            <div className="stardb-app">
-                <Header />
-                <RandomPlanet />
-                <PeoplePage />
+            <SwapiServiceProvider value={this.swapiService}>
+                <div className="stardb-app">
+                    <Header />
+                    <RandomPlanet />
+                    <PeoplePage />
 
-                <PersonDetails itemId={10}></PersonDetails>
-                <PlanetDetails itemId={10}></PlanetDetails>
-                <StarshipDetails itemId={5}></StarshipDetails>
+                    <PersonDetails itemId={10}></PersonDetails>
+                    <PlanetDetails itemId={10}></PlanetDetails>
+                    <StarshipDetails itemId={5}></StarshipDetails>
 
-                <PersonList />
-                <PlanetList />
-                <StarshipList />
-
-            </div>
-
+                    <PersonList />
+                    <PlanetList />
+                    <StarshipList />
+                </div>
+            </SwapiServiceProvider>
         );
     }
 
