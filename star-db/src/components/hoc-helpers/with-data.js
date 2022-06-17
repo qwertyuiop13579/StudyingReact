46d7ef25397d20fa9f163d5ed718346966d67e14
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-17 11:12:12 +0300

    add hoc component

---
+++ star-db/src/components/hoc-helpers/with-data.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Spinner from "../spinner/spinner";
+
+const WithData = (View) => {
+
+    return class extends React.Component {
+
+        state = {
+            data: null,
+        }
+
+        componentDidMount() {
+            const { getData } = this.props;
+
+            getData().then(data => {
+                this.setState({ data });
+            });
+
+        }
+
+        render() {
+            const { data } = this.state;
+
+            if (!data) {
+                return <Spinner />;
+            }
+            return <View {...this.props} data={data} />;
+        }
+    }
+}
+
+export default WithData;
--- star-db/src/components/item-list/item-list.js
+++ star-db/src/components/item-list/item-list.js
@@ -1,58 +1,32 @@
 import React from "react";
 
-import SwapiService from "../../services/swapi-service";
-import Spinner from "../spinner/spinner";
+import { WithData } from '../hoc-helpers';
 
 import './item-list.css';
 
-class ItemList extends React.Component {
+const ItemList = (props) => {
 
-    swapeService = new SwapiService();
-
-    state = {
-        ItemList: null,
-    }
-
-    componentDidMount() {
-
-        const { getData } = this.props;
-        getData().then(ItemList => {
-            this.setState({ ItemList });
-        });
-
-    }
-
-    renderItems(arr) {
-        return arr.map((item) => {
-            const { id } = item;
-            const label = this.props.children(item);
-            return (
-                <li className="list-group-item"
-                    key={id}
-                    onClick={() => this.props.onItemSelected(id)}>
-                    {label}</li>
-            );
-        })
-    }
-
-
-    render() {
-        const { ItemList } = this.state;
-
-        if (!ItemList) {
-            return <Spinner />;
-        }
-
-        const items = this.renderItems(ItemList);
-
+    const { data, onItemSelected, children: renderLabel } = props;
+    const items = data.map((item) => {
+        const { id } = item;
+        const label = renderLabel(item);
         return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
+            <li className="list-group-item"
+                key={id}
+                onClick={() => onItemSelected(id)}>
+                {label}</li>
+        );
+    })
 
-        )
-    }
+    return (
+        <ul className="item-list list-group">
+            {items}
+        </ul>
+
+    )
+
 
 }
 
-export default ItemList;
+
+export default WithData(ItemList);
