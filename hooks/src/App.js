76b5af1fc957062e980ead34a42dd2b7bacad411
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-20 17:19:15 +0300

    use useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Hello world!
-        </p>
+    <div>
+      <HoolSwitcher></HoolSwitcher>
+    </div>
+  );
+}
+
+const HoolSwitcher = () => {
+
+  const [color, setColor] = useState('white');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{ padding: '10px', backgroundColor: color, fontSize: `${fontSize}px` }}>
+      <p>Hello world!</p>
+      <button onClick={() => setColor('white')}>White</button>
+      <button onClick={() => setColor('black')}>Black</button>
+      <button onClick={() => setFontSize((s) => s + 1)}>+size</button>
     </div>
   );
 }
