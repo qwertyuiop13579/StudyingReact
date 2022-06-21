253ec00bac7fe890a5d6aff3d063953943ccc49f
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-21 23:42:43 +0300

    add simple store

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,15 +1,38 @@
-console.log('Hello world!');
+import { configureStore } from '@reduxjs/toolkit';
 
 const reducer = (state = 0, action) => {
 
     switch (action.type) {
-        case 'INC': return state + 1;
+
+        case 'INC':
+            return state + 1;
+
+        case 'DEC':
+            return state - 1;
+
+        case 'RND':
+            return state + action.payload;
+
         default: return state;
     }
 };
 
-let state = reducer(undefined, {});
-state = reducer(state, { type: 'INC' });
-console.log(state);
-state = reducer(state, { type: 'INC' });
-console.log(state);
+const store = configureStore({ reducer: reducer });
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({ type: 'INC' });
+});
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' });
+});
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({ type: 'RND', payload });
+});
+
+const update = () => {
+    document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(update);
+
