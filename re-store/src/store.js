5c41db6952423d0405f8de6470665800025fb901
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-23 16:43:22 +0300

    use store enhancer

--- re-store/src/store.js
+++ re-store/src/store.js
@@ -1,6 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers';
 
-const store = configureStore({ reducer: reducer });
+const stringEnhancer = (configureStore) => (...args) => {
+    const store = configureStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action == 'string') {
+            return originalDispatch({ type: action });
+        }
+        return originalDispatch(action);
+    }
+    return store;
+}
+
+const logEnhancer = (configureStore) => (...args) => {
+    const store = configureStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        console.log(action.type);
+        return originalDispatch(action);
+    }
+    return store;
+}
+
+const store = configureStore({ reducer: reducer, enhancers: [stringEnhancer, logEnhancer] });
+
+store.dispatch('Hello');
 
 export default store;
