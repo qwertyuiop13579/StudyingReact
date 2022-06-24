8583d922a6613b5c81ca649b1b32cbe47e25c2b6
qwertyuiop13579 <harlap1999@mail.ru>
2022-06-24 17:45:46 +0300

    use mode in webpack

--- build-sandbox/webpack.config.js
+++ build-sandbox/webpack.config.js
@@ -2,66 +2,73 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    },
-    module: {
-        rules: [
+module.exports = (env) => {
+    const isProd = env.mode == 'production';
+    const isDev = env.mode == 'development';
 
-            // Babel for JS files
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
+    const getFirstStyleLoader = () => {
+        return isProd ? MiniCssExtractPlugin.loader : 'style-loader';
+    }
+    return {
+        mode: isProd ? 'production' : 'development',
+        entry: './src/index.js',
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        },
+        module: {
+            rules: [
 
-            // Images
-            {
-                test: /\.(png|jpe?g|gif|ico)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'images',
-                        name: '[name]-[sha1:hash:7].[ext]'
-                    }
-                }]
-            },
+                // Babel for JS files
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
 
-            // Fonts
-            {
-                test: /\.(ttf|otf|eot|woff2)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'fonts',
-                        name: '[name].[ext]'
-                    }
-                }]
-            },
+                // Images
+                {
+                    test: /\.(png|jpe?g|gif|ico)$/i,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }]
+                },
 
-            // CSS and Style loaders
-            {
-                test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', "sass-loader",]
-            },
+                // Fonts
+                {
+                    test: /\.(ttf|otf|eot|woff2)$/i,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
+                        }
+                    }]
+                },
+
+                // CSS and Style loaders
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [getFirstStyleLoader(), 'css-loader', "sass-loader",]
+                },
+            ]
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index_bundle.js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'public/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'main-[hash:7].css'
+            }),
         ]
-    },
-    // mode: 'development',
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main-[hash:7].css'
-        }),
-    ]
+    }
 }
